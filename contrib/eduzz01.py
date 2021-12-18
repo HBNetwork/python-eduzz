a24e098551ccf424c930db2934bec01bda48fd61
Henrique Bastos <henrique@bastos.net>
2021-12-18 20:35:16 -0300

    Fix the first example.

--- contrib/eduzz01.py
+++ contrib/eduzz01.py
@@ -1,12 +1,12 @@
 from pprint import pprint
 
 from decouple import config
+
 from eduzz import Eduzz
-from eduzz.auth import EduzzToken
 
-credentials = dict(email=config("EDUZZ_EMAIL"), publickey=config("EDUZZ_PUBLICKEY"), apikey=config("EDUZZ_APIKEY"))
-
-client = Eduzz(EduzzToken(**credentials))
+client = Eduzz.from_credentials(
+    email=config("EDUZZ_EMAIL"), publickey=config("EDUZZ_PUBLICKEY"), apikey=config("EDUZZ_APIKEY")
+)
 
 g = client.get_sales_list("2021-11-01", "2021-12-15")
 
