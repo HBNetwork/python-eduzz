7377fd741ba1c9e15073f60fde1e73ff376d8ade
Henrique Bastos <henrique@bastos.net>
2022-09-07 20:09:21 -0300

    add: CallersHintedArgs class

--- eduzz/magic.py
+++ eduzz/magic.py
@@ -1,9 +1,56 @@
 import functools
-from typing import get_type_hints
+import gc
+import inspect
+from types import FunctionType
+from typing import Union, get_origin, get_type_hints
 
 from cattr import Converter
 
 DEFAULT_CONVERTER = Converter()
+
+
+class CallersHintedArgs:
+    @staticmethod
+    def get_caller_and_locals(previous=0):
+        """Previous defines how far back on the stack we go.
+        0 is the current frame (inside this function).
+        1 is the caller.
+        2 and on get's the callers before.
+        """
+        try:
+            # Get the propper frame.
+            frame = inspect.currentframe()
+            for _ in range(previous):
+                frame = frame.f_back
+
+            localns = frame.f_locals
+            fcode = frame.f_code
+
+            # Searches for the first reference to the related code object.
+            function = next(f for f in gc.get_referrers(fcode) if isinstance(f, FunctionType))
+        finally:
+            del frame
+
+        return function, localns
+
+    @staticmethod
+    def hints(function, localns):
+        hints = get_type_hints(function, localns=localns)
+        rtype = hints.pop("return", None)
+        return hints, rtype
+
+    @staticmethod
+    def filter_args(localns, hints):
+        is_undefined = lambda value, t: get_origin(t) is Union and value is None  # noqa E731
+
+        return {k: localns[k] for k, t in hints.items() if not is_undefined(localns[k], t)}
+
+    @classmethod
+    def informed(cls, previous=2):
+        """This function should be called by the service method."""
+        function, localns = cls.get_caller_and_locals(previous=previous)
+        arg_hints, rtype = cls.hints(function, localns)
+        return cls.filter_args(localns, arg_hints)
 
 
 def autostructure(f=None, *, converter=DEFAULT_CONVERTER):
---
+++ tests/test_paramdict.py
@@ -0,0 +1,58 @@
+from datetime import date
+
+from eduzz.magic import CallersHintedArgs
+
+
+class A:
+    """Fixture class for tests."""
+
+    def method(self, a=1, b=2):
+        return CallersHintedArgs.get_caller_and_locals(previous=1)
+
+    @staticmethod
+    def static(a=1, b=2):
+        return CallersHintedArgs.get_caller_and_locals(previous=1)
+
+    @classmethod
+    def clsmethod(cls, a=1, b=2):
+        return CallersHintedArgs.get_caller_and_locals(previous=1)
+
+
+class TestGetCallerAndLocals:
+    def test_on_functions(self):
+        def f(a=1, b=2):
+            return CallersHintedArgs.get_caller_and_locals(previous=1)
+
+        assert f() == (f, dict(a=1, b=2))
+
+    def test_on_methods(self):
+        instance = A()
+        function = instance.method.__func__
+        assert instance.method() == (function, dict(a=1, b=2, self=instance))
+
+    def test_on_staticmethods(self):
+        function = A.static
+        assert A.static() == (function, dict(a=1, b=2))
+
+    def test_on_classmethods(self):
+        function = A.clsmethod.__func__
+        assert A.clsmethod() == (function, dict(cls=A, a=1, b=2))
+
+
+def test_callers_params_as_dict():
+    def caller(
+        start_date: date,
+        end_date: date,
+        page: int | None = None,
+        contract_id: int | None = None,
+        affiliate_id: int | None = None,
+        content_id: int | None = None,
+        invoice_status: str | None = None,
+        client_email: str | None = None,
+        date_type: str | None = None,
+        parallel=True,
+        kwargs: dict = None,
+    ) -> dict:
+        return CallersHintedArgs.informed()
+
+    assert caller(start_date=1, end_date=2, page=3) == dict(start_date=1, end_date=2, page=3)
