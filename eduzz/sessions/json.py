b7fb71ecf552649683d8c896090d88d72c8aca8f
Henrique Bastos <henrique@bastos.net>
2022-10-08 18:23:59 -0300

    Create a json module to ease access to custom serializers.

---
+++ eduzz/sessions/json.py
@@ -0,0 +1,6 @@
+from eduzz.sessions.json_session import (  # noqa: F401
+    BetterJSONDecoder,
+    BetterJSONEncoder,
+)
+from eduzz.sessions.json_session import deserialize as loads  # noqa: F401
+from eduzz.sessions.json_session import serialize as dumps  # noqa: F401
